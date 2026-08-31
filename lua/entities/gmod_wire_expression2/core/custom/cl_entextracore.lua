16254b66422f804a45fa4d461686a085d9440121
Sir Papate <sirpapate@gmail.com>
2026-08-30 23:11:08 -0400

    feat: add limits, duplicator support, and network improvements to E2 entextracore
    Introduce ConVars to limit tags, key-values, world tip length, and halo blur,
    enhancing server robustness and preventing abuse. Integrate support for
    duplicator saving and loading of custom entity data (tags, key-values, halos,
    world tips). Refactor client-server synchronization for halos and world tips
    to use direct networking, improving efficiency and reliability, and add a
    client-side sync request on `InitPostEntity`.
    
    Correct `haveTag` and `haveKeyValue` functions to return arrays of entities
    instead of booleans, matching their updated descriptions. Fix the description
    for the `children` E2 function. Implement consistent snake_case naming for
    internal variables and functions, and add helper functions for managing entity
    modifiers. Entities with world tips now display a halo on hover.

--- lua/entities/gmod_wire_expression2/core/custom/cl_entextracore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_entextracore.lua
@@ -1,16 +1,16 @@
-E2Helper.Descriptions["runOnEntitySpawn"] = "If set to 1, E2 will run when an entity is spawned."
-E2Helper.Descriptions["entitySpawnClk"] = "Returns the entity that was spawned."
-E2Helper.Descriptions["runOnEntityRemove"] = "If set to 1, E2 will run when an entity is removed."
-E2Helper.Descriptions["entityRemoveClk"] = "Returns the entity that was removed."
+E2Helper.Descriptions["runOnEntitySpawn"] = "If set to 1, E2 will run when an entity is spawned. Deprecated: use the entityCreated event instead"
+E2Helper.Descriptions["entitySpawnClk"] = "Returns the entity that was spawned. Deprecated: use the entityCreated event instead"
+E2Helper.Descriptions["runOnEntityRemove"] = "If set to 1, E2 will run when an entity is removed. Deprecated: use the entityRemoved event instead"
+E2Helper.Descriptions["entityRemoveClk"] = "Returns the entity that was removed. Deprecated: use the entityRemoved event instead"
 
-E2Helper.Descriptions["creationID"] = "Returns the entity's creation ID."
-E2Helper.Descriptions["children"] = "Returns entity's creation ID. Unlike E:id(), it will always increase and old values won't be reused."
+E2Helper.Descriptions["creationID"] = "Returns the entity's creation ID. Unlike E:id(), it will always increase and old values won't be reused."
+E2Helper.Descriptions["children"] = "Returns the entity's children."
 
 E2Helper.Descriptions["addTag"] = "Adds a tag to the entity or all entities in the array. The tag can be used to identify the entity in other E2s."
 E2Helper.Descriptions["removeTag"] = "Removes a tag from the entity or all entities in the array."
 E2Helper.Descriptions["getTags"] = "Gets all tags of the entity."
 E2Helper.Descriptions["hasTag"] = "Returns 1 if the entity has the tag, 0 otherwise."
-E2Helper.Descriptions["haveTag"] = "Returns an array of 1s and 0s. 1 if the entity has the tag, 0 otherwise."
+E2Helper.Descriptions["haveTag"] = "Returns an array of entities that have the tag."
 E2Helper.Descriptions["getEntitiesByTag"] = "Returns an array of entities with the tag."
 
 E2Helper.Descriptions["setKeyValue"] = "Sets a key-value pair on the entity or all entities in the array. The key-value pair can be used to store data on the entity and can be retrieved by other E2s."
@@ -18,7 +18,7 @@ E2Helper.Descriptions["removeKeyValue"] = "Removes a key-value pair from the ent
 E2Helper.Descriptions["getKeyValue"] = "Gets the value of a key-value pair on the entity."
 E2Helper.Descriptions["getKeyValues"] = "Gets all key-value pairs on the entity."
 E2Helper.Descriptions["getEntitiesByKeyValue"] = "Gets all entities with the specified key-value pair."
-E2Helper.Descriptions["haveKeyValue"] = "Returns an array of 1s and 0s. 1 if the entity has the key-value pair, 0 otherwise."
+E2Helper.Descriptions["haveKeyValue"] = "Returns an array of entities that have the specified key-value pair."
 
 E2Helper.Descriptions["setHalo"] = "Applies a halo glow effect to the entity. (Color, BlurX, BlurY, Add, IgnoreZ)"
 E2Helper.Descriptions["removeHalo"] = "Removes the halo from the entity."
@@ -28,73 +28,74 @@ E2Helper.Descriptions["removeWorldTip"] = "Removes the world tip from the entity
 
 -- Halo
 
-local entshalo = {}
+local ents_halo = {}
+local world_tip_halo_ent = NULL
+
+local function readHalo()
+	return {
+		Color = net.ReadColor(),
+		BlurX = net.ReadFloat(),
+		BlurY = net.ReadFloat(),
+		Additive = net.ReadBool(),
+		IgnoreZ = net.ReadBool()
+	}
+end
 
 net.Receive("wire_expression2_entextracore_halo_update", function()
-    local ent = net.ReadEntity()
-    local halo = util.JSONToTable(net.ReadString())
+	local ent = net.ReadEntity()
+	local haloData = readHalo()
 
-    if IsValid(ent) then
-        entshalo[ent] = halo
-    end
+	if IsValid(ent) then
+		ents_halo[ent] = haloData
+	end
 end)
 
 net.Receive("wire_expression2_entextracore_halo_sync", function()
-    local count = net.ReadInt(32)
+	local count = net.ReadUInt(16)
 
-    entshalo = {}
+	ents_halo = {}
 
-    for i = 1, count do
-        local ent = net.ReadEntity()
-        local halo = util.JSONToTable(net.ReadString())
+	for _ = 1, count do
+		local ent = net.ReadEntity()
+		local haloData = readHalo()
 
-        if IsValid(ent) then
-            entshalo[ent] = halo
-        end
-    end
+		if IsValid(ent) then
+			ents_halo[ent] = haloData
+		end
+	end
 end)
 
 net.Receive("wire_expression2_entextracore_halo_remove", function()
-    local ent = net.ReadEntity()
+	local ent = net.ReadEntity()
 
-    if IsValid(ent) then
-        entshalo[ent] = nil
-    end
-end)
-
-hook.Add("PreDrawHalos", "wire_expression2_entextracore_halo_draw", function()
-    for ent, h in pairs(entshalo) do
-        if IsValid(ent) then
-            halo.Add({ent}, h.Color, h.BlurX, h.BlurY, 5, h.Additive, h.IgnoreZ)
-        else
-            entshalo[ent] = nil
-        end
-    end
+	if IsValid(ent) then
+		ents_halo[ent] = nil
+	end
 end)
 
 -- World Tip
 
-local entsworldtip = {}
+local ents_world_tip = {}
 
 net.Receive("wire_expression2_entextracore_worldtip_update", function()
-    local ent = net.ReadEntity()
-    local text = net.ReadString()
+	local ent = net.ReadEntity()
+	local text = net.ReadString()
 
-    if IsValid(ent) then
-        if text ~= "" then
-            entsworldtip[ent] = text
-        else
-            entsworldtip[ent] = nil
-        end
-    end
+	if IsValid(ent) then
+		if text ~= "" then
+			ents_world_tip[ent] = text
+		else
+			ents_world_tip[ent] = nil
+		end
+	end
 end)
 
 net.Receive("wire_expression2_entextracore_worldtip_sync", function()
-    local count = net.ReadInt(32)
+	local count = net.ReadUInt(16)
 
-    entsworldtip = {}
+	ents_world_tip = {}
 
-    for i = 1, count do
+	for _ = 1, count do
         local ent = net.ReadEntity()
         local text = net.ReadString()
 
@@ -108,19 +109,43 @@ net.Receive("wire_expression2_entextracore_worldtip_sync", function()
     end
 end)
 
+hook.Add("InitPostEntity", "wire_expression2_entextracore_sync", function()
+	net.Start("wire_expression2_entextracore_sync_request")
+	net.SendToServer()
+end)
+
 hook.Add("Think", "wire_expression2_entextracore_worldtip_draw", function()
-    local tr = LocalPlayer():GetEyeTrace()
-    local ent = tr.Entity
+	world_tip_halo_ent = NULL
 
-    if IsValid(ent) and entsworldtip[ent] then
-        if tr.StartPos:Distance(tr.HitPos) <= 256 then
-            if ent:IsPlayer() then
-                AddWorldTip(ent:EntIndex(), entsworldtip[ent], 0.5, ent:EyePos())
-            else
-                AddWorldTip(ent:EntIndex(), entsworldtip[ent], 0.5, ent:GetPos())
-            end
+	local lp = LocalPlayer()
+	if not IsValid(lp) then return end
 
-    		halo.Add({ent}, Color(255, 255, 255, 255), 1, 1, 1, true, true)
-        end
-    end
+	local tr = lp:GetEyeTrace()
+	local ent = tr.Entity
+
+	if IsValid(ent) and ents_world_tip[ent] then
+		if tr.StartPos:Distance(tr.HitPos) <= 256 then
+			if ent:IsPlayer() then
+				AddWorldTip(ent:EntIndex(), ents_world_tip[ent], 0.5, ent:EyePos())
+			else
+				AddWorldTip(ent:EntIndex(), ents_world_tip[ent], 0.5, ent:GetPos())
+			end
+
+			world_tip_halo_ent = ent
+		end
+	end
+end)
+
+hook.Add("PreDrawHalos", "wire_expression2_entextracore_halo_draw", function()
+	for ent, h in pairs(ents_halo) do
+		if IsValid(ent) then
+			halo.Add({ent}, h.Color, h.BlurX, h.BlurY, 1, h.Additive, h.IgnoreZ)
+		else
+			ents_halo[ent] = nil
+		end
+	end
+
+	if IsValid(world_tip_halo_ent) then
+		halo.Add({world_tip_halo_ent}, Color(255, 255, 255, 255), 1, 1, 1, true, true)
+	end
 end)
